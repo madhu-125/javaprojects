6cda4d15a638c2e06a4aa5a2037f31d5e55c9e01
madhu-125 <aravamadhu618@gmail.com>
2026-09-14 23:38:56 +0530

    "commit"

---
+++ src/ExamQuestions/Exam5.java
@@ -0,0 +1,24 @@
+package ExamQuestions;
+
+public class Exam5 {
+ 
+	static int count;
+	Exam5(){
+		count++;
+		
+	}
+	public static void main (String $ [] ){
+		 
+		Exam5 e = new Exam5();
+		Exam5 e1 = new Exam5();
+		Exam5 e2= new Exam5();
+		Exam5 e3 = new Exam5();
+		Exam5 e4 = new Exam5();
+		Exam5 e5 = new Exam5();
+		Exam5 es = new Exam5();
+		Exam5 e1s = new Exam5();
+		 
+		System.out.println(count);
+	}
+
+}
