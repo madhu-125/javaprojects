2baeb435e35b3687434ea855210b385d1caf53b6
madhu-125 <aravamadhu618@gmail.com>
2026-09-18 00:51:22 +0530

    "method"

--- src/MethodTest2.java
+++ src/MethodTest2.java
@@ -31,12 +31,12 @@ public class MethodTest2 {
 
 	static void addition() {
 		System.err.println("display the sum of numbers from 1 to 10");
-		int sum = 0; 
-		for (int i = 1; i <=10 ; i++) {
-			sum = sum+= i;
+		int sum = 0;
+		for (int i = 1; i <= 10; i++) {
+			sum += i;
 			System.out.println(sum);
 		}
-			
 	}
+//	
 
 }
